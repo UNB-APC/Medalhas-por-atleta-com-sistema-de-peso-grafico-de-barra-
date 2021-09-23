fd3030e643eff909c182a42a9f077b98c495264f
pedrosena21 <pedrosbhy93@gmail.com>
2021-09-23 13:29:54 -0300

    Falta achar um modo de selecionar o ano e montar o DASH

--- plotly_medalhas.py
+++ plotly_medalhas.py
@@ -1,47 +1,39 @@
+from pandas.core.frame import DataFrame
 import plotly.express as px
 import pandas as pd
 
 #leitura dos dados
-dados_df = pd.read_csv("athlete_events.csv") 
+dados_csv = pd.read_csv("athlete_events.csv") 
 
 #ID[0],"Name"[1],"Sex"[2],"Age"[3],"Height"[4],"Weight"[5],"Team"[6],"NOC"[7],"Games"[8],"Year"[9],"Season"[10],"City"[11],"Sport"[12],"Event"[13],"Medal"[14]
 
-anos = {}
+dados = {}
 
-for linha in dados_df.values:  
+for linha in dados_csv.values:  
     if type(linha[14]) != str:
         continue
     nome= linha[1]
     ano= linha[9]
     medalha= linha[14]
     
-    if anos.get(ano) == None:
-        anos[ano] = {}
-    if anos[ano].get(nome) == None:
-        anos[ano][nome] = {'Gold': 0, 'Silver': 0, 'Bronze':0 }
+    if dados.get(ano) == None:
+        dados[ano] = {}
+    if dados[ano].get(nome) == None:
+        dados[ano][nome] = {'Gold': 0, 'Silver': 0, 'Bronze':0 }
 
-    anos[ano][nome][medalha]+=1
+    dados[ano][nome][medalha]+=1
 
-anoslist = list(anos.keys())
-anoslist.sort()
+anos_ordenados  = sorted(dados)
 
-
- 
-dados = {
-    'atletas' : [],
-    'Gold' : [],  
-    'Silver' : [],  
-    'Bronze' : []  
-}
-for j in range(len(anos[1992])): 
-        dados['atletas'].append(list(anos[1992].keys())[j])
-        dados['Gold'].append(list(anos[1992].values())[j]['Gold'])
-        dados['Silver'].append(list(anos[1992].values())[j]['Silver'])
-        dados['Bronze'].append(list(anos[1992].values())[j]['Bronze'])
-
+dataFrame= {'ano': [], 'atleta': [], 'medalhas':[]}
+for ano in dados:
+    for atleta in dados[ano]:
+        dataFrame['ano'].append(ano)
+        dataFrame['atleta'].append(atleta)
+        dataFrame['medalhas'].append(dados[ano][atleta])
 
 fig = px.bar(
-        dados,
+        dataFrame,
         title="Olimpíadas - Medalhas por atleta",  
         x= dados['atletas'],
         y= [dados['Gold'],dados['Silver'],dados['Bronze']], 
